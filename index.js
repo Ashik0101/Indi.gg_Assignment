2ff27ccadaf3d831ff98c2c120649121a817c2be
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-10-05 21:49:39 +0530

    db configuration done, user model, user controller and user router done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/UserRoutes");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+//user
+app.use("/api", userRouter);
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB!");
+  } catch (error) {
+    console.log("Error Connecting to DB! ", error);
+  }
+
+  console.log(`Server is listening on PORT ${process.env.PORT}`);
+});
